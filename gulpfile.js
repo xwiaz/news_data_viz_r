ad472d4f765f438d477d18aa670a1e360f7451b8
kookas <pabatco@gmail.com>
2017-01-31 15:19:49 -0800

    gulp-connect, livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	concat = require('gulp-concat'),
+	connect = require('gulp-connect'),
 	sass = require('gulp-sass');
 	
 
@@ -30,12 +31,14 @@ gulp.task('styles', function(){
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(dev))
 		.pipe(gulp.dest(dist))
+		.pipe(connect.reload())
 });
 
 gulp.task('js', function(){
 	gulp.src('process/js/*.js')
 		.pipe(concat('script.js'))
 		.pipe(gulp.dest('dev/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
@@ -43,4 +46,11 @@ gulp.task('watch', function(){
 	gulp.watch('process/js/*.js', ['js']);
 });
 
-gulp.task('default', ['log', 'styles', 'js', 'watch']);
+gulp.task('connect', function(){
+	connect.server({
+		root : 'dev',
+		livereload: true
+	});
+});
+
+gulp.task('default', ['log', 'styles', 'js', 'connect', 'watch']);
